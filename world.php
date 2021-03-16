a96fb1e9284ca1e437cf6575fcee0c46abefed52
sisirrag <mca549@rajagiri.edu>
2021-03-17 03:36:37 +0530

    m

--- world.php
+++ world.php
@@ -32,13 +32,13 @@
         </ul>
         <form class="d-flex">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-dark" type="submit">Search</button>
+          <button class="btn btn-outline-light" type="submit">Search</button>
         </form>
       </div>
     </div>
   </nav>
 
-<nav class="navbar navbar-dark bg-dark">
+<nav class="navbar navbar-light bg-light">
 <div class="container-fluid">
   <a class="navbar-brand" href="index.html">
     <img src="https://www.bmw.co.uk/etc/clientlibs/digitals2/clientlib/media/img/BMW_White_Logo.svg" alt="" width="140" height="35" class="d-inline-block align-top">
@@ -52,7 +52,7 @@
     <div class="container">
       <div class="row">
         <div class="col">
-          <div class="card bg-dark text-white">
+          <div class="card bg-light text-white">
           <img src="img (2).jpg" class="card-img" alt="...">
           <div class="card-img-overlay">
             <h5 class="card-title">Sheer Driving Experience</h5>
@@ -63,69 +63,69 @@
       </div> 
       </div><br><br>
       <div class="row">
-        <div class="col"><div class="card bg-dark text-white">
+        <div class="col"><div class="card bg-light text-white">
           <img src="img (3).jpg" class="card-img" alt="...">
           <div class="card-img-overlay">
             <h5 class="card-title">Sheer Driving Experience</h5>
             <p class="card-text">Join The BMW Legacy</p>
-            <p class="card-text"><a class="btn btn-outline-dark" href="book.php" role="button">Book Now</a></p>
+            <p class="card-text"><a class="btn btn-outline-light" href="book.php" role="button">Book Now</a></p>
           </div>
         </div></div>
-        <div class="col"><div class="card bg-dark text-white">
+        <div class="col"><div class="card bg-light text-white">
           <img src="img (4).jpg" class="card-img" alt="...">
           <div class="card-img-overlay">
             <h5 class="card-title">Sheer Driving Experience</h5>
             <p class="card-text">Join The BMW Legacy</p>
-            <p class="card-text"><a class="btn btn-outline-dark" href="book.php" role="button">Book Now</a></p>          </div>
+            <p class="card-text"><a class="btn btn-outline-light" href="book.php" role="button">Book Now</a></p>          </div>
         </div></div>
       </div><br><br>
       <div class="row">
-        <div class="col"><div class="card bg-dark text-white">
+        <div class="col"><div class="card bg-light text-white">
           <img src="img (5).jpg" class="card-img" alt="...">
           <div class="card-img-overlay">
             <h5 class="card-title">Sheer Driving Experience</h5>
             <p class="card-text">Join The BMW Legacy</p>
-            <p class="card-text"><a class="btn btn-outline-dark" href="book.php" role="button">Book Now</a></p>          </div>
+            <p class="card-text"><a class="btn btn-outline-light" href="book.php" role="button">Book Now</a></p>          </div>
         </div></div>
-        <div class="col"><div class="card bg-dark text-white">
+        <div class="col"><div class="card bg-light text-white">
           <img src="img (6).jpg" class="card-img" alt="...">
           <div class="card-img-overlay">
             <h5 class="card-title">Sheer Driving Experience</h5>
             <p class="card-text">Join The BMW Legacy</p>
-            <p class="card-text"><a class="btn btn-outline-dark" href="book.php" role="button">Book Now</a></p>          </div>
+            <p class="card-text"><a class="btn btn-outline-light" href="book.php" role="button">Book Now</a></p>          </div>
         </div></div>
       </div><br><br>
       <div class="row">
-        <div class="col"><div class="card bg-dark text-white">
+        <div class="col"><div class="card bg-light text-white">
           <img src="img (7).jpg" class="card-img" alt="...">
           <div class="card-img-overlay">
             <h5 class="card-title">SHeer Driving Experience</h5>
             <p class="card-text">Join The BMW Legacy</p>
-<p class="card-text"><a class="btn btn-outline-dark" href="book.php" role="button">Book Now</a></p>          </div>
+<p class="card-text"><a class="btn btn-outline-light" href="book.php" role="button">Book Now</a></p>          </div>
         </div></div>
         
       </div><br><br>
       <div class="row">
-        <div class="col"><div class="card bg-dark text-white">
+        <div class="col"><div class="card bg-light text-white">
           <img src="img (7).jpg" class="card-img" alt="...">
           <div class="card-img-overlay">
             <h5 class="card-title">Sheer Driving Experience</h5>
             <p class="card-text">Join The BMW Legacy</p>
-<p class="card-text"><a class="btn btn-outline-dark" href="book.php" role="button">Book Now</a></p>          </div>
+<p class="card-text"><a class="btn btn-outline-light" href="book.php" role="button">Book Now</a></p>          </div>
         </div></div>
-        <div class="col"><div class="card bg-dark text-white">
+        <div class="col"><div class="card bg-light text-white">
           <img src="img (9).jpg" class="card-img" alt="...">
           <div class="card-img-overlay">
             <h5 class="card-title">Sheer Driving Experience</h5>
             <p class="card-text">Join The BMW Legacy</p>
-<p class="card-text"><a class="btn btn-outline-dark" href="book.php" role="button">Book Now</a></p>          </div>
+<p class="card-text"><a class="btn btn-outline-light" href="book.php" role="button">Book Now</a></p>          </div>
         </div></div>
-        <div class="col"><div class="card bg-dark text-white">
+        <div class="col"><div class="card bg-light text-white">
           <img src="img (10).jpg" class="card-img" alt="...">
           <div class="card-img-overlay">
             <h5 class="card-title">Sheer Driving Experience</h5>
             <p class="card-text">Join The BMW Legacy</p>
-<p class="card-text"><a class="btn btn-outline-dark" href="book.php" role="button">Book Now</a></p>          </div>
+<p class="card-text"><a class="btn btn-outline-light" href="book.php" role="button">Book Now</a></p>          </div>
         </div></div>
       </div><br><br>
     </div>
